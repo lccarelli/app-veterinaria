0a66303d8591f3aaaa621f6daadffcd839757b5f
Carelli <laura.carelli@redb.ee>
2019-07-14 20:59:50 -0300

    estructura del formulario

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -6,9 +6,7 @@ class NuevaCita extends Component {
         return (
             <div className = "card mt-5 py-5">
                 <div className ="card-body">
-                    <h2 className = "card-title text-center mb-5">
-                        Llena el formulario para crear una cita
-                    </h2>
+                    <h2 className = "card-title text-center mb-5">Llena el formulario para crear una cita</h2>
 
                     <form>
                        <div className = "form-group row">
@@ -51,6 +49,37 @@ class NuevaCita extends Component {
                                     />
                            </div>
                        </div>
+
+                       <div className = "form-group row">
+                           <label className = "col-sm-4 col-lg-2 col-form-label">
+                               Hora
+                           </label>
+                           <div className = "col-sm-8 col-lg-4">
+                               <input 
+                                    type="time"
+                                    className="form-control"
+                                    name="hora-cita"
+                                    />
+                           </div>
+                       </div>
+
+                       <div className = "form-group row">
+                           <label className = "col-sm-4 col-lg-2 col-form-label">
+                               Hora
+                           </label>
+                           <div className = "col-sm-8 col-lg-10">
+                               <textarea
+                                className = "form-control"
+                                name = "sintomas"
+                                placeholder = "describe los sintomas"
+                               ></textarea>
+                           </div>
+                       </div>
+
+                       <input 
+                        type = "submit" 
+                        className = "py-3 mt-2 btn btn-success btn-block"
+                        value = "Agregar Nueva Cita"/>
                     
                     </form>
                 </div>
