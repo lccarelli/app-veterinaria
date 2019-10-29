f892fddb8051fceb5fbb120fecfa046af5650804
lccarelli <carelli.lc@gmail.com>
2019-10-29 12:19:13 -0300

    trabajando con proptypes

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import PropTypes from 'prop-types';
 
 const stateInicial = {
     cita : {
@@ -145,6 +146,10 @@ class NuevaCita extends Component {
             </div>
         );
     }
+}
+
+NuevaCita.propTypes = {
+    crearNuevaCita : PropTypes.func.isRequired
 }
 
 export default NuevaCita;
