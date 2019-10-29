5629918ea7c86ba99984096395ab69ca159cfc95
lccarelli <carelli.lc@gmail.com>
2019-10-29 11:51:27 -0300

    agregando localstorage para citas

--- src/App.js
+++ src/App.js
@@ -11,11 +11,26 @@ class App extends Component {
     citas: []
   }
 
+  //cuando la aplicacion carga
+  componentDidMount(){
+    const citasLS = localStorage.getItem('citas');
+    if(citasLS) {
+      this.setState({
+        citas : JSON.parse(citasLS)
+      })
+    }
+  }
+
+  //cuando eliminamos o agregamos una nueva cita
+  componentDidUpdate(){
+    localStorage.setItem('ctias', JSON.stringify(this.state.citas));
+  }
+
   crearNuevaCita = datos => {
     //copiar el state actual
     const citas = [...this.state.citas, datos];
 
-    //agregar nuevo state
+    //agregar nuevo states
     this.setState({
       citas
     });
